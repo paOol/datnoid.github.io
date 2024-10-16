1582bc1504e16d9713fe9ae70d8400540b0319eb
paOol <paoolkim@gmail.com>
2024-10-16 09:11:27 -0700

    title/favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,9 @@ import { ThemeProvider } from '@/components/layout/theme-provider';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Shadcn - Landing template',
-  description: 'Landing template from Shadcn',
+  title: 'Datnoid | Safe and Fair trading in web3.',
+  description:
+    'Stop losing money in web3. Datnoid is the safest DEX in crypto; Discover, trade, and create tokens with our Fair Launch System.',
 };
 
 export default function RootLayout({
