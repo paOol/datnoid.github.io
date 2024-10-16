db0694b39bac57e872872479999d25f160babad2
paOol <paoolkim@gmail.com>
2024-10-16 12:46:32 -0700

    footer links

--- components/layout/sections/footer.tsx
+++ components/layout/sections/footer.tsx
@@ -1,6 +1,6 @@
-import { Separator } from "@/components/ui/separator";
-import { ChevronsDownIcon } from "lucide-react";
-import Link from "next/link";
+import { Separator } from '@/components/ui/separator';
+import { ChevronsDownIcon } from 'lucide-react';
+import Link from 'next/link';
 
 export const FooterSection = () => {
   return (
@@ -9,71 +9,26 @@ export const FooterSection = () => {
         <div className="grid grid-cols-2 md:grid-cols-4 xl:grid-cols-6 gap-x-12 gap-y-8">
           <div className="col-span-full xl:col-span-2">
             <Link href="#" className="flex font-bold items-center">
-              <ChevronsDownIcon className="w-9 h-9 mr-2 bg-gradient-to-tr from-primary via-primary/70 to-primary rounded-lg border border-secondary" />
+              <img src="navlogo.svg" style={{ marginRight: '1rem' }} />
 
-              <h3 className="text-2xl">Shadcn</h3>
+              <h3 className="text-2xl">Datnoid</h3>
             </Link>
-          </div>
-
-          <div className="flex flex-col gap-2">
-            <h3 className="font-bold text-lg">Contact</h3>
-            <div>
-              <Link href="#" className="opacity-60 hover:opacity-100">
-                Github
-              </Link>
-            </div>
-
-            <div>
-              <Link href="#" className="opacity-60 hover:opacity-100">
-                Twitter
-              </Link>
-            </div>
-
-            <div>
-              <Link href="#" className="opacity-60 hover:opacity-100">
-                Instagram
-              </Link>
-            </div>
-          </div>
-
-          <div className="flex flex-col gap-2">
-            <h3 className="font-bold text-lg">Platforms</h3>
-            <div>
-              <Link href="#" className="opacity-60 hover:opacity-100">
-                iOS
-              </Link>
-            </div>
-
-            <div>
-              <Link href="#" className="opacity-60 hover:opacity-100">
-                Android
-              </Link>
-            </div>
-
-            <div>
-              <Link href="#" className="opacity-60 hover:opacity-100">
-                Web
-              </Link>
-            </div>
           </div>
 
           <div className="flex flex-col gap-2">
             <h3 className="font-bold text-lg">Help</h3>
             <div>
-              <Link href="#" className="opacity-60 hover:opacity-100">
-                Contact Us
+              <Link
+                href="https://docs.datnoid.com/"
+                className="opacity-60 hover:opacity-100"
+              >
+                Docs
               </Link>
             </div>
 
             <div>
-              <Link href="#" className="opacity-60 hover:opacity-100">
+              <Link href="#faq" className="opacity-60 hover:opacity-100">
                 FAQ
-              </Link>
-            </div>
-
-            <div>
-              <Link href="#" className="opacity-60 hover:opacity-100">
-                Feedback
               </Link>
             </div>
           </div>
@@ -81,38 +36,33 @@ export const FooterSection = () => {
           <div className="flex flex-col gap-2">
             <h3 className="font-bold text-lg">Socials</h3>
             <div>
-              <Link href="#" className="opacity-60 hover:opacity-100">
-                Twitch
+              <Link
+                href="https://t.me/weirdfrens"
+                className="opacity-60 hover:opacity-100"
+              >
+                Telegram
               </Link>
             </div>
 
             <div>
-              <Link href="#" className="opacity-60 hover:opacity-100">
+              <Link
+                href="https://discord.gg/HNH6ZehFaY"
+                className="opacity-60 hover:opacity-100"
+              >
                 Discord
               </Link>
             </div>
 
             <div>
-              <Link href="#" className="opacity-60 hover:opacity-100">
-                Dribbble
+              <Link
+                href="https://twitter.com/Datnoid_Dapp"
+                className="opacity-60 hover:opacity-100"
+              >
+                Twitter
               </Link>
             </div>
           </div>
         </div>
-
-        <Separator className="my-6" />
-        <section className="">
-          <h3 className="">
-            &copy; 2024 Designed and developed by
-            <Link
-              target="_blank"
-              href="https://github.com/leoMirandaa"
-              className="text-primary transition-all border-primary hover:border-b-2 ml-1"
-            >
-              Leo Miranda
-            </Link>
-          </h3>
-        </section>
       </div>
     </footer>
   );
