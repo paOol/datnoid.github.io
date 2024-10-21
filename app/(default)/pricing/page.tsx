79259525f5262bbbc00f65906df1ef2b34d3f954
paOol <paoolkim@gmail.com>
2024-10-20 17:09:37 -0700

    assets

--- app/(default)/pricing/page.tsx
+++ app/(default)/pricing/page.tsx
@@ -1,13 +1,13 @@
 export const metadata = {
   title: 'Pricing - Stellar',
   description: 'Page description',
-}
+};
 
-import PricingSection from './pricing-section'
-import Features from '@/components/features-05'
-import Customers from '@/components/customers'
-import Faqs from '@/components/faqs'
-import Cta from '@/components/cta'
+import PricingSection from './pricing-section';
+import Features from '@/components/features-05';
+import Customers from '@/components/customers';
+import Faqs from '@/components/faqs';
+import Cta from '@/components/cta';
 
 export default function Pricing() {
   return (
@@ -18,5 +18,5 @@ export default function Pricing() {
       <Faqs />
       <Cta />
     </>
-  )
+  );
 }
