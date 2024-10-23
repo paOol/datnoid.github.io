43560dc359d91cf731e573f98b3c674dfb45ca5a
paOol <paoolkim@gmail.com>
2024-10-23 12:15:25 -0700

    add text logo, hide 'about' page

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -1,4 +1,6 @@
 import Link from 'next/link';
+import Image from 'next/image';
+
 import Logo from './logo';
 import MobileMenu from './mobile-menu';
 
@@ -8,22 +10,28 @@ export default function Header() {
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
         <div className="flex items-center justify-between h-16 md:h-20">
           {/* Site branding */}
-          <div className="flex-1">
+          <div className="flex-1 flex items-center space-x-2">
             <Logo />
+            <Image
+              src={'https://app.datnoid.com/datnoidlogo.svg'}
+              alt="Datnoid"
+              width={200}
+              height={40}
+            />
           </div>
 
           {/* Desktop navigation */}
           <nav className="hidden md:flex md:grow">
             {/* Desktop menu links */}
             <ul className="flex grow justify-center flex-wrap items-center">
-              <li>
+              {/* <li>
                 <Link
                   className="font-medium text-sm text-slate-300 hover:text-white mx-4 lg:mx-5 transition duration-150 ease-in-out"
                   href="/about"
                 >
                   About
                 </Link>
-              </li>
+              </li> */}
               <li>
                 <Link
                   className="font-medium text-sm text-slate-300 hover:text-white mx-4 lg:mx-5 transition duration-150 ease-in-out"
