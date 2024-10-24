7d03bcf26ff9e16d786fe2059e9912eec2e898cd
paOol <paoolkim@gmail.com>
2024-10-24 08:30:19 -0700

    analytics script

--- app/(default)/layout.tsx
+++ app/(default)/layout.tsx
@@ -1,39 +1,41 @@
-'use client'
+'use client';
 
-import { useEffect } from 'react'
+import { useEffect } from 'react';
 
-import AOS from 'aos'
-import 'aos/dist/aos.css'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
-import Header from '@/components/ui/header'
-import Footer from '@/components/ui/footer'
+import Header from '@/components/ui/header';
+import Footer from '@/components/ui/footer';
+import Script from 'next/script';
 
 export default function DefaultLayout({
   children,
 }: {
-  children: React.ReactNode
-}) {  
-
+  children: React.ReactNode;
+}) {
   useEffect(() => {
     AOS.init({
       once: true,
       disable: 'phone',
       duration: 1000,
       easing: 'ease-out-cubic',
-    })
-  })
+    });
+  });
 
   return (
     <>
+      <Script
+        strategy="afterInteractive"
+        src="https://didiavax.com/script.js"
+        data-website-id="461e4f40-6e0d-4252-9205-8e64aa9f0db2"
+      />
+
       <Header />
-      
-      <main className="grow">
 
-        {children}
-
-      </main>
+      <main className="grow">{children}</main>
 
       <Footer />
     </>
-  )
+  );
 }
