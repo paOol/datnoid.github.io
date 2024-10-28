150d9ff6df20f750bd53e1b11eef408aef72e145
paOol <paoolkim@gmail.com>
2024-10-28 15:45:44 -0700

    about page

--- app/(default)/about/page.tsx
+++ app/(default)/about/page.tsx
@@ -1,24 +1,25 @@
 export const metadata = {
   title: 'About - Datnoid',
-  description: 'We prevent scams for token traders and generate better returns for liquidity providers on our unified liquidity DEX.',
-}
+  description:
+    'We prevent scams for token traders and generate better returns for liquidity providers on our unified liquidity DEX.',
+};
 
-import Hero from '@/components/hero-about'
-import Story from '@/components/story'
-import Team from '@/components/team'
-import Recruitment from '@/components/recruitment'
-import Testimonials from '@/components/testimonials-02'
-import Cta from '@/components/cta-02'
+import Hero from '@/components/hero-about';
+import Story from '@/components/story';
+import Team from '@/components/team';
+import Recruitment from '@/components/recruitment';
+import Testimonials from '@/components/testimonials-02';
+import Cta from '@/components/cta-02';
 
 export default function About() {
   return (
     <>
       <Hero />
       <Story />
-      <Team />
-      <Recruitment />
-      <Testimonials />
+      {/* <Team /> */}
+      {/* <Recruitment /> */}
+      {/* <Testimonials /> */}
       <Cta />
     </>
-  )
+  );
 }
